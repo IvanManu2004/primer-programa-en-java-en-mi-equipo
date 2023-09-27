46fb2366c45bf8e56efb5115073eb2c8ce42b9e1
Iván <ivanmanu2004@gmail.com>
2023-09-27 13:38:37 +0200

    Apellidos, año de nacimiento y edad

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,8 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Fernández Fernández");	
+		System.out.println("Fernández Fernández");
+		System.out.println("Naci en el 2004 y tengo 19 años");	
 
 	}
 
