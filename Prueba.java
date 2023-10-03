f985fbd87be6df803de98357049c5601931ee3ff
Iván <ivanmanu2004@gmail.com>
2023-10-03 13:09:00 +0200

    Nueva linea con musica favorita

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Fernández Fernández");
 		System.out.println("Naci en el 2004 y tengo 19 años");	
 		System.out.println("Mi comida fav es Pasta a la carbonara");
+		System.out.println("Mi Música favorita es la tecno");
 
 	}
 
